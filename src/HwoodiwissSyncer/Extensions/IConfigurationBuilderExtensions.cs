af0b13251896adc76018e4d50a1b6d6a467367e2
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2024-09-18 00:12:09 +0100

    Remove appsettings.Secrets.json

--- src/HwoodiwissSyncer/Extensions/IConfigurationBuilderExtensions.cs
+++ src/HwoodiwissSyncer/Extensions/IConfigurationBuilderExtensions.cs
@@ -4,6 +4,5 @@ public static class IConfigurationBuilderExtensions
 {
     public static IConfigurationBuilder ConfigureConfiguration(this IConfigurationBuilder configurationBuilder) =>
         configurationBuilder
-            .AddJsonFile("appsettings.Secrets.json")
             .AddUserSecrets<Program>();
 }
